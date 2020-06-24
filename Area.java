8fec9447b6b8251b19c562a38a4e6ceb0cf7204c
vidhimishra625 <66908016+vidhimishra625@users.noreply.github.com>
2020-06-24 21:19:29 +0530

    Create Area.java

---
+++ Area.java
@@ -0,0 +1,37 @@
+public class Program 
+{
+    public static void main(String[] args)
+    {
+        System.out.println(area(5.0));
+        System.out.println(area(-1));
+        System.out.println(area(5.0,4.0));
+        System.out.println(area(-1.0,4.0));
+    }
+    public static double area(double r)
+    {
+        double PI=Math.PI;
+        double a;
+        if(r>=0)
+        {
+            a=r*r*PI;
+            return a;
+        }
+        else
+        {
+            return (-1);
+        }
+    }
+    public static double area(double x, double y)
+    {
+        double a;
+        if(x>=0&&y>=0)
+        {
+            a=x*y;
+            return a;
+        }
+        else
+        {
+            return (-1);
+        }
+    }
+}
